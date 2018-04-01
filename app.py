0f9cb51482ec3e71f0905948cd499455f5f7dd9c
longjj <luojj26@mail2.sysu.edu.cn>
2018-04-01 19:34:01 +0800

    dev: finsh json response in home page

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+import json
+from flask.json import jsonify
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Flask inside Docker!!"
 
+@app.route('/', methods=['GET'])
+def get_index_json():
+    filename = os.path.join(app.instance_path, 'menu_database.json')
+    f = open(filename, encoding='utf-8')
+    res = json.load(f)
+    return jsonify(res)
+
+
+# @app.route('/confirm', methods=['POST'])
+# def buy():
+#     blablabla...
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
