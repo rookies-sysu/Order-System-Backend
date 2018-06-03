42c0908793542669bf8f35bb38aa32e4cc81f7c7
longjj <luojj26@mail2.sysu.edu.cn>
2018-06-03 15:09:49 +0800

    db can connect backup

--- db/tools.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-import json
-
-
-def get_config(file_name="config"):
-    """Get Configuration"""
-    with open(file_name, "r", encoding="utf-8") as f:
-        config = json.load(f)
-    return config
