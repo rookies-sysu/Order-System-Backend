58fbafd011d86774f2aea1512fd903d53a58d9ea
YungLinjy <jingyanglin96@gmail.com>
2018-07-08 21:13:29 +0800

    fix test of RecommendationDetails

--- tests/test_db_recommendationdetails.py
+++ tests/test_db_recommendationdetails.py
@@ -7,19 +7,28 @@ class TestDbRecommendationDetails(unittest.TestCase):
         
     def test_insert(self):
         self.rcdOpt = RecommendationDetailsOperator()
-        self.rcdOpt.insertRecommendationDetailsItem(
+        state = self.rcdOpt.insertRecommendationDetailsItem(
             recommendationID=6, dishID=10, description="balabalabala...")
-        self.rcdOpt.insertRecommendationDetailsItem(
-            recommendationID=3, dishID=12, description="balabalabala...")
-        self.rcdOpt.insertRecommendationDetailsItem(
-            recommendationID=4, dishID=10, description="balabalabala...")
-        self.rcdOpt.insertRecommendationDetailsItem(
-            recommendationID=3, dishID=10, description="balabalabala...")
-        self.rcdOpt.insertRecommendationDetailsItem(
+        self.assertEqual(False, state)
+        state = self.rcdOpt.insertRecommendationDetailsItem(
             recommendationID=3, dishID=11, description="balabalabala...")
+        self.assertEqual(False, state)
+        state = self.rcdOpt.insertRecommendationDetailsItem(
+            recommendationID=4, dishID=10, description="balabalabala...")
+        self.assertEqual(False, state)
+        state = self.rcdOpt.insertRecommendationDetailsItem(
+            recommendationID=4, dishID=8, description="balabalabala...")
+        self.assertEqual(True, state)
+        state = self.rcdOpt.insertRecommendationDetailsItem(
+            recommendationID=4, dishID=9, description="balabalabala...")
+        self.assertEqual(True, state)
     def test_delete(self):
         self.rcdOpt = RecommendationDetailsOperator()
-        self.rcdOpt.deleteRecommendationDetailsByRecommendationID(recommendationID=8)
-        self.rcdOpt.deleteRecommendationDetailsByDishID(dishID=12)
-        self.rcdOpt.deleteRecommendationDetailsByDishID(dishID=10)
-        self.rcdOpt.deleteRecommendationDetailsByRecommendationID(recommendationID=3)
+        state = self.rcdOpt.deleteRecommendationDetailsByRecommendationID(recommendationID=8)
+        self.assertEqual(False, state)
+        state = self.rcdOpt.deleteRecommendationDetailsByDishID(dishID=11)
+        self.assertEqual(False, state)
+        state = self.rcdOpt.deleteRecommendationDetailsByDishID(dishID=9)
+        self.assertEqual(True, state)
+        state = self.rcdOpt.deleteRecommendationDetailsByRecommendationID(recommendationID=4)
+        self.assertEqual(True, state)
