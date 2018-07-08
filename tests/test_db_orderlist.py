2499ead690fb0f7fc6a050a6a328c6de82025b8a
Nition <864206745@qq.com>
2018-07-08 12:45:26 +0800

    Update test_db_orderlist.py

--- tests/test_db_orderlist.py
+++ tests/test_db_orderlist.py
@@ -5,9 +5,26 @@ class TestDbOrderList(unittest.TestCase):
     def setUp(self):
         print('setUp...')
         self.opt = orderListOperator()
+        self.opt.manageOrderListTable(restaurantName='rName1', password='123456')
 
-    def test_insert(self):
-        pass
+    def test_insert_and_delete(self):
+        #插入用于辅助测试的table
+        tOpt = tableOperator(restaurantName='rName1', password='123456')
+        tOpt.insertTableItem(tableNumber=10)
+        _, table = selectOperator(tableName="RestaurantTable", tableNumber=10, result=["tableID"])
 
-    def test_delete(self):
-        pass
+        #插入order
+        number = self.opt.insertOrderItem(orderDetail="testOrderDetail", total=10.0, 
+                                    customerID="testCustomerID", tableID=table[0]['tableID'])
+        if (number != -1):
+            self.assertEqual(True, True)
+
+        _, orderId = selectOperator(tableName="OrderList", orderNumber=number, result=["orderID"])
+        #首次删除order
+        state = self.opt.deleteOrderItemWithOrderID(orderID=orderId[0]['orderID'])
+        self.assertEqual(True, state)
+        #重复删除order
+        state = self.opt.deleteOrderItemWithOrderID(orderID=orderId[0]['orderID'])
+        self.assertEqual(False, state)
+
+        tOpt.deleteTableByNumber(tableNumber=10)
