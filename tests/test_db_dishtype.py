f6c702edfcf60b7e793d60b05fc80f974f5073a5
Nition <864206745@qq.com>
2018-07-08 12:44:56 +0800

    Update test_db_dishtype.py

--- tests/test_db_dishtype.py
+++ tests/test_db_dishtype.py
@@ -5,9 +5,18 @@ class TestDbDishType(unittest.TestCase):
     def setUp(self):
         print('setUp...')
         self.opt = dishTypeOperator()
+        self.opt.manageDishTypeTable(restaurantName='rName1', password='123456')
 
-    def test_insert(self):
-        pass
-
-    def test_delete(self):
-        pass
+    def test_insert_and_delete(self):
+        #首次插入dishtype
+        state = self.opt.insertDishTypeItem(dishTypeName="testDishType")
+        self.assertEqual(True, state)
+        #重复插入dishtype
+        state = self.opt.insertDishTypeItem(dishTypeName="testDishType")
+        self.assertEqual(False, state)
+        #首次删除dishtype
+        state = self.opt.deleteDishTypeByName(dishTypeName="testDishType")
+        self.assertEqual(True, state)
+        #重复删除dishtype
+        state = self.opt.deleteDishTypeByName(dishTypeName="testDishType")
+        self.assertEqual(False, state)
