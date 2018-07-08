f42c075adfddcda73e634f9a6a4c654261b4795c
Nition <864206745@qq.com>
2018-07-08 14:25:48 +0800

    Update test_db_basic.py

--- tests/test_db_basic.py
+++ tests/test_db_basic.py
@@ -6,7 +6,7 @@ class TestDbBasic(unittest.TestCase):
         print('setUp...')
         self.rOpt = restaurantOperator()
 
-    def test_select(self):
+    def test_select(self):    
         state, res = selectOperator(tableName='Restaurant',
                           restaurantName='testName', result=['restaurantName', 'password', 'phone', 'email'])
         print(state, res)
@@ -16,6 +16,21 @@ class TestDbBasic(unittest.TestCase):
         self.assertEqual('testPhone', res[0]['phone'])
         self.assertEqual('testEmail', res[0]['email'])
 
-    def test_update(self):
-        self.assertEqual(True, True)
+    def test_update(self):      
+        state = updateOperator(rstName='rName1', pwd='1234567', tableName='Restaurant',
+                    restaurantName='rName1', new_phone='10293847569', new_restaurantName='rName2')
+        self.assertEqual(False, state)
+        state = updateOperator(rstName='rName1', pwd='123456', tableName='Restaurant',
+                    restaurantName='rName1', new_password='1234567')
+        self.assertEqual(True, state)
+        state = updateOperator(rstName='rName1', pwd='1234567', tableName='Restaurant',
+                    restaurantName='rName1', new_restaurantName='rName2')
+        self.assertEqual(True, state)
+        state = updateOperator(rstName='rName2', pwd='1234567', tableName='Restaurant',
+                    restaurantName='rName2', new_password='123456')
+        self.assertEqual(True, state)
+        state = updateOperator(rstName='rName2', pwd='123456', tableName='Restaurant',
+                    restaurantName='rName2', new_restaurantName='rName1')
+        self.assertEqual(True, state)
+
         
