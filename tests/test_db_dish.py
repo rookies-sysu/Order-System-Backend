b978c64a5fd3b1cae6e0350b344acb47213959b0
Nition <864206745@qq.com>
2018-07-08 12:44:05 +0800

    Update test_db_dish.py

--- tests/test_db_dish.py
+++ tests/test_db_dish.py
@@ -5,9 +5,31 @@ class TestDbDish(unittest.TestCase):
     def setUp(self):
         print('setUp...')
         self.opt = dishOperator()
+        self.opt.manageDishTable(restaurantName='rName1', password='123456')
 
-    def test_insert(self):
+    def test_insert_and_delete(self):
+        #插入用于辅助测试的dishtype
+        dish_type_opt = dishTypeOperator(restaurantName='rName1', password='123456')
+        dish_type_opt.insertDishTypeItem(dishTypeName="dishtype1")
+        _, dishtype = selectOperator(tableName="DishType", dishTypeName="dishtype1", result=["dishTypeID"])
+        
+        #首次插入该dish
+        state = self.opt.insertDishItem(dishName="testDishName", dishDescription="testDishDescription",
+                                    price=10.0, dishImageURL="testDishImageURL", dishTypeID=dishtype[0]['dishTypeID'])
+        self.assertEqual(True, state)
+        #重复插入该dish
+        state = self.opt.insertDishItem(dishName="testDishName", dishDescription="testDishDescription",
+                                    price=10.0, dishImageURL="testDishImageURL", dishTypeID=dishtype[0]['dishTypeID'])
+        self.assertEqual(False, state)
+        #删除该dish
+        _, result = selectOperator(tableName="Dish", dishName="testDishName", result=["dishID"])
+        state = self.opt.deleteDishItemWithDishID(result[0]['dishID'])
+        self.assertEqual(True, state)
+        #重复删除dish
+        state = self.opt.deleteDishItemWithDishID(result[0]['dishID'])
+        self.assertEqual(False, state)
+
+        dish_type_opt.deleteDishTypeByName(dishTypeName="dishtype1")
+
         pass
 
-    def test_delete(self):
-        pass
