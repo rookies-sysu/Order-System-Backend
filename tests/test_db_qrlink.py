54b3eac14f290344c5cad9c3828932a698363a8d
Nition <864206745@qq.com>
2018-07-08 12:46:19 +0800

    Update test_db_qrlink.py

--- tests/test_db_qrlink.py
+++ tests/test_db_qrlink.py
@@ -5,9 +5,24 @@ class TestDbQRLink(unittest.TestCase):
     def setUp(self):
         print('setUp...')
         self.opt = QRlinkOperator()
+        self.opt.manageQRlinkTable(restaurantName='rName1', password='123456')
 
-    def test_insert(self):
+    def test_insert_and_delete(self):
+        tOpt = tableOperator(restaurantName='rName1', password='123456')
+        tOpt.insertTableItem(tableNumber=1)
+
+        state = self.opt.insertQRlinkItem(linkImageURL="testlinkImageURL", tableNumber=1)
+        self.assertEqual(True, state)
+        state = self.opt.insertQRlinkItem(linkImageURL="testlinkImageURL", tableNumber=1)
+        self.assertEqual(False, state)
+        state = self.opt.deleteLinkByURL(linkImageURL="testlinkImageURL")
+        self.assertEqual(True, state)
+        state = self.opt.deleteLinkByURL(linkImageURL="testlinkImageURL")
+        self.assertEqual(False, state)
+
+        tOpt.deleteTableByNumber(tableNumber=1)
+
         pass
 
-    def test_delete(self):
-        pass
+    # def test_delete(self):
+    #     pass
